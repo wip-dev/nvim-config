1420e2543b176e49e3f4b1c1149efaeaeb71724f
wip-dev <wipdev.tech@gmail.com>
2023-08-04 13:19:48 +0300

    some terminal remaps

--- lua/waseem/remap.lua
+++ lua/waseem/remap.lua
@@ -26,16 +26,26 @@ local term_group = vim.api.nvim_create_augroup("TermGroup", { clear = true })
 vim.api.nvim_create_autocmd("TermEnter", {
     group = term_group,
     callback = function()
+        -- pnpm
         vim.keymap.set('t', ';pi', 'pnpm install', { buffer = true })
         vim.keymap.set('t', ';ps', 'pnpm start', { buffer = true })
         vim.keymap.set('t', ';prd', 'pnpm run dev', { buffer = true })
         vim.keymap.set('t', ';prb', 'pnpm run build', { buffer = true })
+
+        -- git
         vim.keymap.set('t', ';gs', 'git status', { buffer = true })
         vim.keymap.set('t', ';gd', 'git diff', { buffer = true })
         vim.keymap.set('t', ';ga', 'git add .', { buffer = true })
         vim.keymap.set('t', ';gc', 'git commit -m ""<Left>', { buffer = true })
+
+        -- clj
+        vim.keymap.set('t', ';scw', 'npx shadow-cljs watch app', { buffer = true })
+        vim.keymap.set('t', ';lr', 'lein run', { buffer = true })
+
+        -- utils
         vim.keymap.set('t', ';ap', 'sudo apt update', { buffer = true })
         vim.keymap.set('t', ';ag', 'sudo apt upgrade', { buffer = true })
+        vim.keymap.set('t', ';tr', 'tree -I node_modules', { buffer = true })
     end
 })
 
@@ -44,6 +54,7 @@ vim.keymap.set('n', '<leader>wd', function() vim.cmd.cd("%:h") end,
     { desc = 'Set [w]orking [d]irectory to current file' })
 vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [k]eymaps' })
 vim.keymap.set('n', '<C-s>', ':w<Enter>', { desc = 'Write/Save' })
-vim.keymap.set('n', '<leader>vt', ':vsp<Enter>:term<Enter>i', { desc = 'Start [Terminal] (in [V]ertical split)' })
+vim.keymap.set('n', '<leader>tv', ':vsp<Enter>:term<Enter>i', { desc = 'Start [Terminal] (in [V]ertical split)' })
+vim.keymap.set('n', '<leader>th', ':sp<Enter>:term<Enter>i', { desc = 'Start [Terminal] (in [H]orizontal split)' })
 vim.keymap.set('i', '<C-s>', '<Esc>:w<Enter>a', { desc = 'Write/Save' })
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Back to Normal Mode' })
