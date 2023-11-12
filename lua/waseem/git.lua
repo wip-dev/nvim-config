f6410786f4599b66e8db823f617462fec3dd8791
wipdev-tech <wipdev.tech@gmail.com>
2023-11-12 15:54:55 +0200

    Added git branch mapping

--- lua/waseem/git.lua
+++ lua/waseem/git.lua
@@ -1,6 +1,7 @@
 local main_git_callback = function()
     vim.keymap.set('n', '<leader>gs', ":Git<Enter>", { buffer = true })
     vim.keymap.set('n', '<leader>gd', ":Git diff", { buffer = true })
+    vim.keymap.set('n', '<leader>gb', ":Git branch<Enter>", { buffer = true })
     vim.keymap.set('n', '<leader>ga', ":Git add .", { buffer = true })
     vim.keymap.set('n', '<leader>gc', ":Git commit<Enter>", { buffer = true })
     vim.keymap.set('n', '<leader>gl', ":Git log<Enter>", { buffer = true })
