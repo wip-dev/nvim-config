52edec4917ee10e7fea69de4ba5a8fb8f91cb499
wipdev-tech <wipdev.tech@gmail.com>
2024-03-23 19:15:09 +0200

    Lualine tweaks

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -14,7 +14,20 @@ require('lualine').setup({
     options = {
         icons_enabled = true,
         theme = 'catppuccin',
-        component_separators = { left = ' ', right = ' ' },
-        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+    },
+    sections = {
+        lualine_a = {
+            {
+                'mode',
+                cond = nil,            -- Condition function, the component is loaded when the function returns `true`.
+                draw_empty = false,    -- Whether to draw component even if it's empty.
+                padding = 1,           -- Adds padding to the left and right of components.
+                fmt = function(str, _) -- Format function, formats the component's output.
+                    return " " .. str
+                end,
+            }
+        }
     }
 })
